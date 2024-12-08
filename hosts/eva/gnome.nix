00f972040fd456f10e8436348d8331a452c70cf1
VitalyR <vr@vitalyr.com>
2024-12-08 15:50:41 +0800

    feat: add gnome module for eva

---
+++ hosts/eva/gnome.nix
@@ -0,0 +1,9 @@
+{pkgs-unstable, ...}: {
+  environment.systemPackages = with pkgs-unstable; [
+    gnomeExtensions.appindicator
+    gnome-extension-manager
+    gnomeExtensions.quake-terminal
+    gnomeExtensions.clipboard-history
+    gnomeExtensions.kimpanel
+  ];
+}
