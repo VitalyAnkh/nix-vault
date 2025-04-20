73ac8aadfa47e4bebdb40c4ee86ebded5c2b83b5
VitalyR <vr@vitalyr.com>
2025-04-20 15:24:04 +0800

    Revert "feat: remove emacs"
    This reverts commit 49ed072d25bf81e9d4c8432c71904d2f29cf4a6a.

--- hosts/darwin-harmonica/home.nix
+++ hosts/darwin-harmonica/home.nix
@@ -1,21 +1,16 @@
-{ config, ... }:
-let
-  hostName = "fern";
-in
-{
+_: {
   programs.ssh = {
     enable = true;
     extraConfig = ''
       Host github.com
         Hostname github.com
-        # github is controlled by fern~
-        IdentityFile ~/.ssh/fern
+        # github is controlled by harmonica~
+        IdentityFile ~/.ssh/harmonica
         # Specifies that ssh should only use the identity file explicitly configured above
         # required to prevent sending default identity files first.
         IdentitiesOnly yes
     '';
   };
-
   modules.editors.emacs = {
     enable = true;
   };
