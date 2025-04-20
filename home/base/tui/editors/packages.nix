ecaaa0d85a5d9790b9a0cc9e8a5892eb18e23e05
VitalyR <vr@vitalyr.com>
2025-04-20 15:24:04 +0800

    Revert "feat: remove emacs"
    This reverts commit 49ed072d25bf81e9d4c8432c71904d2f29cf4a6a.

--- home/base/tui/editors/packages.nix
+++ home/base/tui/editors/packages.nix
@@ -79,6 +79,15 @@
             pyquery
             pyyaml
             boto3
+
+            ## emacs's lsp-bridge dependenciesge
+            # epc
+            # orjson
+            # sexpdata
+            # six
+            # setuptools
+            # paramiko
+            # rapidfuzz
           ]
       ))
 
