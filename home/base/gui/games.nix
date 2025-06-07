0654718d78288f5a029d0a892902897d7802261b
VitalyR <vr@vitalyr.com>
2025-06-07 23:25:23 +0800

    feat: add cataclysm-dda-git

--- home/base/gui/games.nix
+++ home/base/gui/games.nix
@@ -5,8 +5,14 @@
 }:
 {
   home.packages = with pkgs-unstable; [
-    gamescope # SteamOS session compositing window manager
     prismlauncher # A free, open source launcher for Minecraft
     winetricks # A script to install DLLs needed to work around problems in Wine
+
+    # some games
+    beyond-all-reason
+    cataclysm-dda-git
+    zeroad-unwrapped
+    zeroad-data
+    nix-gaming.packages.${pkgs-unstable.system}.osu-lazer-bin
   ];
 }
